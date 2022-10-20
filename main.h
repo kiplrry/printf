c85cda125fcdd67191d068c903ba3cebf7f1b2ce
Mayeku Khisa <101919651+mayekukhisa@users.noreply.github.com>
2022-10-20 22:53:54 +0300

    add header

---
+++ main.h
@@ -0,0 +1,27 @@
+#ifndef PRINT_F
+#define PRINT_F
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * struct print - Struct print
+ * @sym: The operator
+ * @f: The function associated
+ */
+struct convert
+{
+char *sym;
+int (*f)(va_list);
+};
+typedef struct convert conver_t;
+
+int parser(const char *format, conver_t f_list[], va_list arg_list);
+int _printf(const char *format, ...);
+int _write_char(char);
+int print_char(va_list);
+int print_string(va_list);
+int print_percent(va_list);
+
+#endif
